2feb9d9e63eb7df65179ecad83ac3d568773ca30
Sidra Jabeen <sidrajabeen5454@gmail.com>
2021-08-25 12:21:46 +0500

    Initial Commit

---
+++ Standard CharteredTests/Standard_CharteredTests.swift
@@ -0,0 +1,33 @@
+//
+//  Standard_CharteredTests.swift
+//  Standard CharteredTests
+//
+//  Created by Sidra Jabeen on 25/08/2021.
+//
+
+import XCTest
+@testable import Standard_Chartered
+
+class Standard_CharteredTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
